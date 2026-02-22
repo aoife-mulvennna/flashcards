b1e5f9ce7d76fa211cb9c8457101ae7554701e30
aoife-mulvennna <aoife.mulvenna10@gmail.com>
2026-02-22 22:13:47 +0000

    Initial monorepo: Spring Boot + Angular + Postgres

---
+++ flashcards-backend/src/main/java/com/aoife/flashcardsbackend/FlashcardsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aoife.flashcardsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardsBackendApplication.class, args);
+    }
+
+}
